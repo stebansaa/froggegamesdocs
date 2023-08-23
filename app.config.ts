d8cf60e00abd1481239351a6a23b4ae56c952ccd
Esteban Saá <steban@gmail.com>
2023-08-22 22:28:18 -0400

    update

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   docus: {
     title: 'The Frogge Game',
     description: 'A unique multiplayer experience that blends survival gameplay, token incentives, and blockchain technology.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'https://www.frogge.finance/assets/logo-8f1a76eb.png',
     socials: {
       twitter: 'frogge.finance',
       github: 'frogge-game/whitepaper',
